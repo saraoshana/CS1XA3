ce195887832457ec44fa6e2e07c1341d9489e855
saraoshana <59624406+saraoshana@users.noreply.github.com>
2020-03-19 08:45:58 -0400

    updated project02.js for merge conflict

--- Project02/Project02.js
+++ Project02/Project02.js
@@ -6,6 +6,40 @@ function topFunction2() { //scroll up
 		document.body.scrollTop = 2000; 
 		document.documentElement.scrollTop = 2000; 
 }
-document.getElementById("changeYellow").onclick = function(){ //change background colour
+document.getElementById("change_Orange").onclick = function(){ //change background colour
 	document.getElementById("output").style.backgroundColor = '#f5cab3';
 }
+
+ //jQuery smooth scroll
+ $('.navbar a').on('click', function(e) {
+	 if (this.hash !== '') {
+		 e.preventDefault();
+		 
+		 const hash = this.hash;
+		 
+		 $('html, body').animate(
+		 {
+			 screenTop:$(hash).offset().top
+		 },
+		 800
+		);
+	 }
+ });
+ //bookmark button
+ function bookmarksite(title,url) { 
+   // Internet Explorer
+   if(document.all)
+   {
+       window.external.AddFavorite(url, title); 
+   }
+   // Google Chrome
+   else if(window.chrome){
+      alert("Press Ctrl+D to add bookmark");
+   }
+   // Firefox
+   else if (window.sidebar)
+   {
+       window.sidebar.addPanel(title, url, ""); 
+   }
+} 
+ 
